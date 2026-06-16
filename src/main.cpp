9d6eca2b453f0b795796b02b4c1f2ad2acedac53
w7h3r <aarifokcan@gmail.com>
2026-06-16 04:03:16 +0300

    Built program's main func

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muokcan <muokcan@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/24 21:17:43 by muokcan           #+#    #+#             */
+/*   Updated: 2026/05/24 21:29:37 by muokcan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Channel/Channel.hpp"
+#include "../inc/Client/Client.hpp"
+#include "../inc/Server/Server.hpp"
+#include "../inc/Exception/Exception.hpp"
+
+int	main(void)
+{
+	Server server;
+
+	try 
+	{
+		server.start();
+	}
+	catch (const std::exception& e) 
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+}
