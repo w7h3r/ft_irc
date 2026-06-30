9c747235aa311e7daca3d71490dd5b45a21c13e1
ArchcyX <alperenlermi0@gmail.com>
2026-07-01 02:06:30 +0300

    feature/server.cpp: Socket init finish

--- src/Server.cpp
+++ src/Server.cpp
@@ -47,7 +47,21 @@ void	Server::server_start()
 
 	if (setsockopt(_socket, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) < 0)
 		throw(std::runtime_error("Error socket option: TCP_NODELAY not set"));
+	
+	struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(_port);
 
+if (bind(_socket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+        throw std::runtime_error("Error: Bind do not success. Ports already in use");
+
+    if (listen(_socket, SOMAXCONN) < 0) //bu kısımda bulunan somaxconn değerini 42 bilgisayarlarına göre güncelle zaten cap işlemi var
+        throw std::runtime_error("Error: listen not working ");
+
+    int flags = fcntl(_socket, F_GETFL, 0);
+    if (flags == -1 || fcntl(_socket, F_SETFL, flags | O_NONBLOCK) == -1)
+        throw std::runtime_error("Error: Non-blocking not set");
 }
 
 
