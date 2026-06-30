002358e2966a76257f0f4b3fd4506d7f785bbe64
ArchcyX <alperenlermi0@gmail.com>
2026-07-01 01:42:51 +0300

    feature/server.cpp: socketopt set

--- src/Server.cpp
+++ src/Server.cpp
@@ -14,6 +14,7 @@
 #include "../inc/Exception/Exception.hpp"
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <exception>
 #include <unistd.h>
 
 Server::Server(int port, const std::string& password) : _port(port),
@@ -33,12 +34,20 @@ Server::~Server()
 
 void	Server::server_start()
 {
+	int	opt = 1;
 	_socket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (_socket == -1)
 		std::cout << "Error Socket Not Open" << std::endl;
 	else
 		std::cout << "Socket Success open" << std::endl;
 	std::cout << _socket << std::endl;
+
+	if (setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+		throw (std::runtime_error("Error socket option:SO_REUSEADDR not set");
+
+	if (setsockopt(_socket, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) < 0)
+		throw(std::runtime_error("Error socket option: TCP_NODELAY not set"));
+
 }
 
 
